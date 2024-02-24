6ac05162c90c04c2c83ab94607ebac70ffdc4fc8
Nikita Bogatyrev <bogatyrevnikita@bk.ru>
2024-02-24 22:18:16 +0400

    Home work 15.4

--- ConsoleApplication2.cpp
+++ ConsoleApplication2.cpp
@@ -1,15 +1,28 @@
 
 #include <iostream>
-#include <string>
+
+int N = 20;
+
+void FindOddOrEvenNumbers(int limit)
+{
+    std::cout << '\n';
+    std::cout << "Enter: \"1\" - odd numbers, \"0\" - even numbers" << '\n';
+    int IsOdd;
+    std::cin >> IsOdd;
+    for (int count = IsOdd; count < limit; count+=2)
+       std::cout << count << " ";
+    std::cout << '\n';
+}
+
 
 int main()
-{
-    std::string  s = "Stroka s tekstom _";
-   
-    std::cout << "String - " << "\"" << s << "\"" << "\n";
-    std::cout << "Line length - " << s.size() << "\n";
-    std::cout << "First character - " << "\"" << s.front() << "\"" << "\n";
-    std::cout << "Last character - " << "\"" << s.back() << "\"" << "\n";
+{ 
+    for (int count = 0; count < N; count += 2)
+        std::cout << count << " ";
 
-    return 0;
+    FindOddOrEvenNumbers(33);
+
+    
+
+   
 }
